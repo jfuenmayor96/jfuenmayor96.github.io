74113cd8b1552f591af0c382104b258c18a68e56
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-06-22 23:06:28 -0400

    projects section added

--- development/src/App.js
+++ development/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import About from './containers/About/About';
 import Main from './containers/Main/Main';
 import Navbar from './containers/Navbar/Navbar';
+import Projects from './containers/Projects/Projects';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div style={{paddingLeft: '50px', paddingRight: '50px'}}>
           <Main/>
           <About/>
+          <Projects/>
         </div>
       </div>
     );
