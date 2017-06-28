067062c51caacc52170a3777b70e964f8973d20d
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-06-28 18:22:57 -0400

    AOS library was initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import AOS from 'aos';
 import About from './containers/About/About';
 import Main from './containers/Main/Main';
 import Navbar from './containers/Navbar/Navbar';
 import Projects from './containers/Projects/Projects';
 import Skills from './containers/Skills/Skills';
+
+AOS.init({
+  duration: '1000',
+  easing: 'ease-in-out',
+});
+
 
 class App extends Component {
   render() {
