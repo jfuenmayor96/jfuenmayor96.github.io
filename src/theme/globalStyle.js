9d612283f90e82028414bae84f3ef11fed924f7f
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-06-28 22:02:32 -0400

    added a global stylesheet

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,25 @@
+import {injectGlobal} from 'styled-components';
+
+/* eslint-disable */
+injectGlobal`
+
+	body {
+
+
+
+		@media screen and (min-width : 320px) {
+			padding: 0px 10px;
+		}
+		@media screen and (min-width : 375px) {
+			padding: 0px 20px;
+		}
+		@media screen and (min-width : 425px) {
+			padding: 0px 30px;
+		}
+		@media screen and (min-width : 768px) {
+			padding: 0px 50px;
+		}
+	}
+
+	
+`;
