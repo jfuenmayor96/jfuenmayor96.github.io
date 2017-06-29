ea1e715294309e46b269801c2206eb3dd1333f8e
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-06-28 22:23:37 -0400

    added padding and margintop to fix anchors from navbar

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -5,19 +5,17 @@ class About extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <div id="about" className="container-fluid">
-                        <div className="row"  data-aos="slide-right" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
-                            <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-                                <H2>About me</H2>
-                            </div>
-                            <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12'>
-                                <P>My name is Julio Fuenmayor and I am 21 years old. Currently I'm studying the 4th, out of 5, year of Computer Science degree at the Universidad Simon Bolivar in Caracas, Venezuela. I'm interested in subjects like information systems, software engineering, web and software development, systems administration and Unix-like environments. I'm a very curious and self-taught person, eager to learn new things in order to succeed in reaching new goals. </P>
-                            </div>
+                <div id="about" className="container-fluid" style={{marginTop: "-57px", paddingTop: "57px"}}>
+                    <div className="row"  data-aos="slide-right" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
+                        <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                            <H2>About me</H2>
+                        </div>
+                        <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12'>
+                            <P>My name is Julio Fuenmayor and I am 21 years old. Currently I'm studying the 4th, out of 5, year of Computer Science degree at the Universidad Simon Bolivar in Caracas, Venezuela. I'm interested in subjects like information systems, software engineering, web and software development, systems administration and Unix-like environments. I'm a very curious and self-taught person, eager to learn new things in order to succeed in reaching new goals. </P>
                         </div>
                     </div>
-                    <HR data-aos="zoom-in" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="0" data-aos-once="true"/>
                 </div>
+                <HR data-aos="zoom-in" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="0" data-aos-once="true"/>
             </div>
         );
     }
