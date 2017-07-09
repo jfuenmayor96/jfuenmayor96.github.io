970511572f2a39acfb3f15322af7f40d0b660778
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-09 18:59:36 -0400

    Updates

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -1,18 +1,11 @@
 
-/* Expresion regular para validar el formato del correo. Espera: nombre@compania.dominio */
-function validEmail(email) {
+function validEmail(email) { // see:
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
   return re.test(email);
 }
-
-function validPhone(phone) {
-  var re = /^(\+{0,1}[0-9]+([\-][0-9]+)*)$/i;
-  return re.test(phone);
-}
-
-// Obtiene toda la informacion del form y devuelve un objeto que la contiene
+// get all data in form and return object
 function getFormData() {
-  var elements = document.getElementById("gform").elements; // Obtiene los elementos del form con id 'gform'
+  var elements = document.getElementById("gform").elements; // all form elements
   var fields = Object.keys(elements).map(function(k) {
     if(elements[k].name !== undefined) {
       return elements[k].name;
@@ -25,7 +18,6 @@ function getFormData() {
   });
   var data = {};
   fields.forEach(function(k){
-    
     data[k] = elements[k].value;
     var str = ""; // declare empty string outside of loop to allow
                   // it to be appended to for each item in the loop
@@ -46,103 +38,66 @@ function getFormData() {
       }
     }
   });
-  // console.log(data);
-  document.getElementById('submit-button-1').style.pointerEvents = 'none';
-  document.getElementById('submit-button-1').style.backgroundColor = '#cccccc';
-  document.getElementById('submit-button-1').style.color = '#666666';
+  console.log(data);
   return data;
 }
 
 function handleFormSubmit(event) {  // handles form submit withtout any jquery
   event.preventDefault();           // we are submitting via xhr below
   var data = getFormData();         // get the values submitted in the form
-  // console.log(data);
-  
-  if( data.Contacto == "" ) {
-    document.getElementById('alertaContacto').style.display = "inherit";
-    document.getElementById('Contacto').value = "";
-    document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-    document.getElementById('submit-button-1').style.backgroundColor= '#202957';
-    document.getElementById('submit-button-1').style.color = 'white';
-    return false;
-  } 
-  else {
-    document.getElementById('alertaContacto').style.display = "none";
-  }
-  if(!validPhone(data.Telefono)) {   
-    document.getElementById('alertaTlfo').style.display = "inherit";
-    document.getElementById('Telefono').value = "";
-    document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-    document.getElementById('submit-button-1').style.backgroundColor= '#202957';
-    document.getElementById('submit-button-1').style.color = 'white';
-    return false;
-  } 
-  else {
-    document.getElementById('alertaTlfo').style.display = "none";
-  }
-  if( !validEmail(data.Email) ) {   
-    document.getElementById('alertaCorreo').style.display = "inherit";
-    document.getElementById('email').value = "";
-    document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-    document.getElementById('submit-button-1').style.backgroundColor= '#202957';
-    document.getElementById('submit-button-1').style.color = 'white';
-    return false;
-  } 
-  else {
-    document.getElementById('alertaCorreo').style.display = "none";
-  }
-  if( data.Mensaje == "" ) {
-    document.getElementById('alertaMensaje').style.display = "inherit";       
-    document.getElementById('mensaje').value = "";
-    document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-    document.getElementById('submit-button-1').style.backgroundColor= '#202957';
-    document.getElementById('submit-button-1').style.color = 'white';
+  if (!data.Contact) {
+    document.getElementById('contactAlert').style.display = 'inline';
     return false;
   }
   else {
-    document.getElementById('alertaMensaje').style.display = "none";
+    document.getElementById('contactAlert').style.display = 'none';
+
   }
-  if (!grecaptcha.getResponse()) {
-    document.getElementById('captchaMessage').style.display = "inherit";
-    document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-    document.getElementById('submit-button-1').style.backgroundColor= '#202957';
-    document.getElementById('submit-button-1').style.color = 'white';
+  if( !data.Email ) {   // if email is not valid show error
+    document.getElementById('emailAlert').style.display = 'inline';
     return false;
-  }   
+  }
   else {
-    document.getElementById('captchaMessage').style.display = "none";
+    document.getElementById('emailAlert').style.display = 'none';
+  }
+  if( !validEmail(data.Email) ) {   // if email is not valid show error
+    document.getElementById('emailAlert2').style.display = 'inline';
+    return false;
+  }
+  else {
+    document.getElementById('emailAlert2').style.display = 'none';
+  }
+  if(!data.Message){
+    document.getElementById('messageAlert').style.display = 'inline';
+    return false;
+  }
+  else{
+    document.getElementById('messageAlert').style.display = 'none';    
+  }
+  if(!grecaptcha.getResponse()){
+    document.getElementById('captchaMessage').style.display = 'inline';
+    return false;
+  }
+  else {
+    document.getElementById('captchaMessage').style.display = 'none';
     var url = event.target.action;  //
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     // xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
-        // console.log( xhr.status, xhr.statusText )
-        // console.log(xhr.responseText);
-        document.getElementById('popup1').style.display = 'inherit';
-        document.getElementById('popup1').style.opacity = '1';
-        document.getElementById('popup1').style.zIndex = '99999999';
-        $("#close-btn").click(function(){
-          document.getElementById('popup1').style.display = 'hidden';
-          document.getElementById('popup1').style.opacity = '0';
-          document.getElementById('popup1').style.zIndex = '-1';
-        });
-        document.getElementById('gform').reset();
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('contactAlert').style.display = 'none';
 
-        document.getElementById('submit-button-1').style.pointerEvents= 'auto';
-        document.getElementById('submit-button-1').style.backgroundColor= '#00AB99';
-        document.getElementById('submit-button-1').style.color = 'white';
+        document.getElementById('emailAlert').style.display = 'none';
 
-        $("#Empresa").attr("placeholder", "Mi Empresa");
-        $("#Contacto").attr("placeholder", "John Doe");
-        $("#Telefono").attr("placeholder", "+584141234567");
-        $("#email").attr("placeholder", "usuario@correo.com");
-        $("#mensaje").attr("placeholder", "Ingrese su mensaje acá.");
-        document.getElementById('alertaContacto').style.display = "hidden";
-        document.getElementById('alertaTlfo').style.display = "hidden";
-        document.getElementById('alertaCorreo').style.display = "hidden";
-        document.getElementById('alertaMensaje').style.display = "hidden";       
-        document.getElementById('captchaMessage').style.display = "hidden";
+        document.getElementById('emailAlert2').style.display = 'none';
+
+        document.getElementById('messageAlert').style.display = 'none';
+
+        document.getElementById('captchaMessage').style.display = 'none';
 
         //document.getElementById('thankyou_message').style.display = 'block';
         return;
