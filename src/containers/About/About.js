94dba3a1e4691b12370f5cecbe0539552261c533
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-09 22:39:28 -0400

    AOS effects duration changed

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -6,17 +6,17 @@ class About extends Component {
         return (
             <div>
                 <div id="about" className="container-fluid" style={{marginTop: "-57px", paddingTop: "57px"}}>
-                    <div className="row"  data-aos="slide-right" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
-                        <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12'>
+                    <div className="row"  >
+                        <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12' data-aos="fade-in-up" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
                             <H2>About me</H2><br/>
                             <P>My name is Julio Fuenmayor and I am 21 years old. Currently I'm studying the 4th, out of 5, year of Computer Science degree at the Universidad Simon Bolivar in Caracas, Venezuela. I'm interested in subjects like information systems, software engineering, web and software development, systems administration and Unix-like environments. I'm a very curious and self-taught person, eager to learn new things in order to succeed in reaching new goals. </P>
                         </div>
-                        <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                            <IMG className="image-responsive pull-right" src={require("../../assets/About/profile_pic.jpg")} style={{height: "240px", width: "240px"}}/>
+                        <div className='col-lg-5 col-md-4 col-sm-12 col-xs-12 text-center' data-aos="fade-in-up" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
+                            <IMG className="image-responsive" src={require("../../assets/About/profile_pic.jpg")} style={{height: "230px", width: "230px"}}/>
                         </div>
                     </div>
                 </div>
-                <HR data-aos="zoom-in" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="0" data-aos-once="true"/>
+                <HR data-aos="zoom-in" data-aos-delay="1000" data-aos-duration="1000" data-aos-offset="0" data-aos-once="true"/>
             </div>
         );
     }
