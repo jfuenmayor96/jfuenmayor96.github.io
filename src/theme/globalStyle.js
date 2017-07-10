ed6fb101163645292e11206a6d4dfaa75340ef60
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-09 22:40:11 -0400

    the size of the recaptcha box is changed to fit mobile screens

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -3,9 +3,12 @@ import {injectGlobal} from 'styled-components';
 /* eslint-disable */
 injectGlobal`
 
+	@media screen and (max-height: 575px){ #rc-imageselect, .g-recaptcha {transform:scale(0.77);-webkit-transform:scale(0.77);transform-origin:0 0;-webkit-transform-origin:0 0;} } 
+	#rc-imageselect {transform:scale(0.77);-webkit-transform:scale(0.77);transform-origin:0 0;-webkit-transform-origin:0 0;} 
+
 	body {
 
-
+		font-family: "Ropa Sans";
 
 		@media screen and (min-width : 320px) {
 			padding: 0px 10px;
