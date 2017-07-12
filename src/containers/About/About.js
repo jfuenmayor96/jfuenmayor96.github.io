ee37b61d7540e197ccd93ae22778c012ef0fce5d
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-12 13:47:15 -0400

    added more information

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -9,7 +9,7 @@ class About extends Component {
                     <div className="row"  >
                         <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12' data-aos="fade-in-up" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
                             <H2>About me</H2><br/>
-                            <P>My name is Julio Fuenmayor and I am 21 years old. Currently I'm studying the 4th, out of 5, year of Computer Science degree at the Universidad Simon Bolivar in Caracas, Venezuela. I'm interested in subjects like information systems, software engineering, web and software development, systems administration and Unix-like environments. I'm a very curious and self-taught person, eager to learn new things in order to succeed in reaching new goals. </P>
+                            <P>My name is Julio Fuenmayor and I am 21 years old. Currently I'm studying the 4th, out of 5, year of Computer Science degree at the Universidad Simon Bolivar in Caracas, Venezuela. I'm interested in subjects like information systems, software engineering, web and software development, systems administration and Unix-like environments. I'm a very curious and self-taught person who likes reading about new tools and technologies. I like to propose efficient solutions to improve any type of process and thus offer final products of high quality.</P>
                         </div>
                         <div className='col-lg-5 col-md-4 col-sm-12 col-xs-12 text-center' data-aos="fade-in-up" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="60" data-aos-once="true">
                             <IMG className="image-responsive" src={require("../../assets/About/profile_pic.jpg")} style={{height: "230px", width: "230px"}}/>
