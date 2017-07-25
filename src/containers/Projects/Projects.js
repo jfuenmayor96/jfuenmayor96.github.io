f2aa67ff090478681027cd72e5ce99e7d6c929d0
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-25 17:43:49 -0400

    fixed some typos

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -28,13 +28,13 @@ class Projects extends Component {
                             <P><b>Repository</b>: <a href='https://github.com/jfuenmayor96/mark-steem-down'>https://github.com/jfuenmayor96/mark-steem-down</a></P>
                         </div>
                     </div>
-                    <div className="row" style={{paddingTop: "15px", paddingBottom: "15px"}}>  
+                    <div className="row" style={{paddingTop: "15px", paddingBottom: "15px"}}>
                         <div className='col-lg-5 col-md-5 col-sm-12 col-xs-12 text-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="320" data-aos-once="true">
                             <IMG className="img-responsive" src={require("../../assets/Projects/agrochannel.png")} height="auto" width="auto"/>
-                        </div>                  
+                        </div>
                         <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12 pull-right' style={{paddingTop: "20px"}}>
                             <H3>Agrochannel.TV</H3>
-                            <P style={{paddingTop: '15px'}}><b>Description</b>: A colombian streaming website who wants to support the population of entrepreneurs of the agricultural and livestock sector in Colombia. They offer a series of streaming shows where the farmers show they work and talk about it.</P>
+                            <P style={{paddingTop: '15px'}}><b>Description</b>: A colombian streaming website who wants to support the population of entrepreneurs of the agricultural and livestock sector in Colombia. They offer a series of streaming shows where the farmers show their work and talk about it.</P>
 
                             <P><b>Developed using</b>: ReactJS, HTML, CSS, Bootstrap</P>
 
@@ -43,10 +43,10 @@ class Projects extends Component {
                             <P><b>Link</b>: <a href='http://agrochannel.tv'> wwww.agrochannel.tv</a></P>
                         </div>
                     </div>
-                    <div className="row" style={{paddingTop: "15px", paddingBottom: "15px"}}>    
+                    <div className="row" style={{paddingTop: "15px", paddingBottom: "15px"}}>
                         <div className='col-lg-5 col-md-5 col-sm-12 col-xs-12 text-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="320" data-aos-once="true">
                             <IMG className="img-responsive" src={require("../../assets/Projects/portfolio.png")} height="auto" width="auto"/>
-                        </div>   
+                        </div>
                         <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12 pull-right' style={{paddingTop: "20px"}}>
                             <H3>Portfolio</H3>
                             <P style={{paddingTop: '15px'}}><b>Description</b>: This is my personal portfolio. The main purpose of it is to have a nice way of showing my work to the people. Here I will publish the projects that I have taken part in and I'm proud of. Also, this portfolio will serve as a personal long-term hobby/distraction in which I will test new techniques and tools that I may discover. The stable version of the portfolio will always be on the master branch of its repository, while the tests and experimental work will be on some other branches. You are free to check my work and progress, and also comment about it. The logo illustration and the design of the site was made by myself. </P>
@@ -63,7 +63,7 @@ class Projects extends Component {
                     <div className="row" style={{paddingTop: "15px", paddingBottom: "15px"}}>
                         <div className='col-lg-7 col-md-8 col-sm-12 col-xs-12 pull-right' style={{paddingTop: "20px"}}>
                             <H3>SIGPIE</H3>
-                            <P style={{paddingTop: '15px'}}><b>Description</b>: Stands for Sistema de Gestion del Programa de Intercambio Estudiantil. SIGPIE is an information system created in the subject PS1116. Its main purpose was to agilize the process of the students applications to the different exchange programs that the Universidad Simon Bolivar maintains with other institutions. It allows the students to register in the program and fill in their personal information as well as attach all the documents required. The personnel in charge can manage this information and download it from the server as a CSV file, reducing significantly the amount of paper and time required to do these tasks manually. It was developed using IBM's RUP in a team of 10 persons.</P>
+                            <P style={{paddingTop: '15px'}}><b>Description</b>: Stands for Sistema de Gestion del Programa de Intercambio Estudiantil. SIGPIE is an information system created in the subject PS1116. Its main purpose was to agilize the process of the students applications to the different exchange programs that the Universidad Simon Bolivar maintains with other institutions. It allows the students to register in the program and fill in their personal information as well as attach all the documents required. The personnel in charge can manage this information and download it from the server as a CSV file, reducing significantly the amount of paper and time required to do these tasks manually. It was developed using IBM's RUP in a team of 10 people.</P>
 
                             <P><b>Developed using</b>: web2py, Python, HTML5, CSS, Bootstrap, Javascript </P>
 
